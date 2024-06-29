f3554cae2206f9f9272496c3a1ae0716d1e51bf3
Wolfgang Meier <wolfgang@existsolutions.com>
2024-06-29 10:27:26 +0200

    Initial setup

---
+++ src/main/java/org/eeditiones/markdown/MarkdownModule.java
@@ -0,0 +1,71 @@
+package org.eeditiones.markdown;
+
+import org.exist.dom.QName;
+import org.exist.xquery.*;
+import org.exist.xquery.value.FunctionParameterSequenceType;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.exist.xquery.FunctionDSL.functionDefs;
+
+/**
+ * A very simple example XQuery Library Module implemented
+ * in Java.
+ */
+public class MarkdownModule extends AbstractInternalModule {
+
+    public static final String NAMESPACE_URI = "https://e-editiones.org/exist-db/markdown";
+    public static final String PREFIX = "md";
+    public static final String RELEASED_IN_VERSION = "eXist-6.2.0";
+
+    // register the functions of the module
+    public static final FunctionDef[] functions = functionDefs(
+        functionDefs(ParsingFunctions.class,
+                ParsingFunctions.FS_HELLO_WORLD,
+                ParsingFunctions.FS_SAY_HELLO,
+                ParsingFunctions.FS_ADD
+        )
+    );
+
+    public MarkdownModule(final Map<String, List<? extends Object>> parameters) {
+        super(functions, parameters);
+    }
+
+    @Override
+    public String getNamespaceURI() {
+        return NAMESPACE_URI;
+    }
+
+    @Override
+    public String getDefaultPrefix() {
+        return PREFIX;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Example Module for eXist-db XQuery";
+    }
+
+    @Override
+    public String getReleaseVersion() {
+        return RELEASED_IN_VERSION;
+    }
+
+    static FunctionSignature functionSignature(final String name, final String description,
+            final FunctionReturnSequenceType returnType, final FunctionParameterSequenceType... paramTypes) {
+        return FunctionDSL.functionSignature(new QName(name, NAMESPACE_URI), description, returnType, paramTypes);
+    }
+
+    static FunctionSignature[] functionSignatures(final String name, final String description,
+            final FunctionReturnSequenceType returnType, final FunctionParameterSequenceType[][] variableParamTypes) {
+        return FunctionDSL.functionSignatures(new QName(name, NAMESPACE_URI), description, returnType, variableParamTypes);
+    }
+
+    static class ExpathBinModuleErrorCode extends ErrorCodes.ErrorCode {
+        private ExpathBinModuleErrorCode(final String code, final String description) {
+            super(new QName(code, NAMESPACE_URI, PREFIX), description);
+        }
+    }
+}
